08a80d29674af59b5881fe9682a49524dcbe1db7
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-15 14:17:06 +0200

    Created logo component

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,13 +1,16 @@
 import Button from '../../components/button/Button.jsx';
 import Loader from '../../components/loader/Loader.jsx';
 import ErrorMessage from '../../components/errorMessage/ErrorMessage.jsx';
+import Logo from '../../components/logo/Logo.jsx';
 import './Home.css';
+
 
 
 function Home() {
     return (
       <div className="outer-content-container">
           <div className="inner-content-container__column">
+              <Logo />
               <h1>Home</h1>
               <Button
                   type="button"
