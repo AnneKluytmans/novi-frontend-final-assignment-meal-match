2bc34c954abffad6a37c8b63ff7b4200c421d068
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-15 13:54:53 +0200

    Created button component

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,12 +1,23 @@
+import Button from '../../components/button/Button.jsx';
 import './Home.css';
-
 
 
 function Home() {
     return (
-      <>
-          <h1>Home</h1>
-      </>
+      <div className="outer-content-container">
+          <div className="inner-content-container">
+              <h1>Home</h1>
+              <Button
+                  type="button"
+                  className="btn__default"
+                  onClick={() => {
+                      console.log("This button is clicked!");
+                  }}
+              >
+                  Try it now
+              </Button>
+          </div>
+      </div>
     );
 }
 
